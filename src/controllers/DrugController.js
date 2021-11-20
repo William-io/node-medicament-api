c88334eec389892aef25fad86d9e8484e02f335f
William-io <capuletos@live.com>
2021-11-19 21:04:16 -0300

    CRUD Completado

--- src/controllers/DrugController.js
+++ src/controllers/DrugController.js
@@ -20,32 +20,78 @@ exports.getBySlug = (req, res, next) => {
             active: true
         }, 'product price slug tags')
         .then(data => {
-            res.status(200).send({ data, message: 'Medicamentos listados com sucesso!' });
+            res.status(200).send(
+                { data, message: 'Medicamentos listados por SLUG com sucesso!' });
         }).catch(e => {
-            res.status(400).send({ e, message: 'Erro ao buscar medicamentos!' });
+            res.status(400).send(
+                { e, message: 'Erro ao buscar medicamentos!' });
+        });
+}
+
+exports.getById = (req, res, next) => {
+    Drug
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send({ data, message: 'Medicamentos listados por ID com sucesso!' });
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getByTag = (req, res, next) => {
+    Drug
+        .find({
+            tags: req.params.tag,
+            active: true
+        }, 'product price slug tags')
+        .then(data => {
+            res.status(200).send({ data, message: 'Medicamentos listados por TAGS com sucesso!' });
+        }).catch(e => {
+            res.status(400).send(e);
         });
 }
 
 exports.post = (req, res, next) => {
     var drug = new Drug(req.body);
-    //Deu tudo certo??
     drug.save().then(x => {
         res.status(201).send({ message: 'Medicamento cadastrado com sucesso!' });
-        //Deu algum erro??
+
     }).catch(e => {
         res.status(400).send({ message: 'Erro ao cadastrar medicamento!', data: e });
     });
-
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
+    Drug
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                product: req.body.product,
+                activePrinciple: req.body.activePrinciple,
+                price: req.body.price,
+            }
+        }).then(x => {
+            res.status(200).send({
+                message: 'Medicamento atualizado com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao atualizar medicamento!',
+                data: e
+            });
+        });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Drug
+        .findOneAndRemove(req.body.id)
+        .then(x => {
+            res.status(200).send({
+                message: 'Medicamento removido com sucesso!'
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: 'Erro ao remover medicamento!',
+                data: e
+            });
+        });
 };
--- src/routes/drug-route.js
+++ src/routes/drug-route.js
@@ -8,6 +8,8 @@ const controller = require('../controllers/DrugController')
 
 router.get('/', controller.get);
 router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tags/:tag', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/', controller.delete);
